ec4df007e831b9ce736b3713db37746af8795ed5
itsnotrin <30768410+itsnotrin@users.noreply.github.com>
2023-01-13 15:12:29 +0000

    Round the ram usage percentage to 2 decimal points.

--- main.go
+++ main.go
@@ -2,10 +2,22 @@ package main
 
 import (
 	"time"
+	"math"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/limiter"
 	"github.com/shirou/gopsutil/v3/mem"
 )
+
+func round(num float64) int {
+    return int(num + math.Copysign(0.5, num))
+}
+
+func toFixed(num float64, precision int) float64 {
+    output := math.Pow(10, float64(precision))
+    return float64(round(num * output)) / output
+}
+
+
 
 func main() {
 	app := fiber.New()
@@ -23,7 +35,7 @@ func main() {
 
 	app.Get("/mem", func(c *fiber.Ctx) error {
 		v, _ := mem.VirtualMemory() // Use mem from the import.
-        var used_percentage = v.UsedPercent
+        var used_percentage = toFixed(v.UsedPercent, 1)
         var usedRam = v.Used/1000/1000 // Calculates it approximately to gigabytes from bytes.
         return c.JSON(fiber.Map{"used_percent": used_percentage, "ram_usage_mb": usedRam})
 	})
