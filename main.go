9ef7ad45e85712f96ee2baa29863b2db1db68c89
itsnotrin <30768410+itsnotrin@users.noreply.github.com>
2023-01-13 15:02:42 +0000

    Clean up- used variables and implemented used memory in MB too.

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"time"
-
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/limiter"
 	"github.com/shirou/gopsutil/v3/mem"
@@ -23,8 +22,10 @@ func main() {
 	}))
 
 	app.Get("/mem", func(c *fiber.Ctx) error {
-		v, _ := mem.VirtualMemory()
-		return c.JSON(fiber.Map{"used_percent": v.UsedPercent})
+		v, _ := mem.VirtualMemory() // Use mem from the import.
+        var used_percentage = v.UsedPercent
+        var usedRam = v.Used/1000/1000 // Calculates it approximately to gigabytes from bytes.
+        return c.JSON(fiber.Map{"used_percent": used_percentage, "ram_usage_mb": usedRam})
 	})
 
 	app.Listen(":3000")
