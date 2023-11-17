8cf7308d09069e2fe7c31a3eea1ddfd62d7e8708
itsnotrin <30768410+itsnotrin@users.noreply.github.com>
2023-11-17 14:37:12 +0000

    More work

--- go.mod
+++ go.mod
@@ -1,22 +1,49 @@
 module github.com/itsnotrin/monity-client
 
-go 1.18
+go 1.21
 
-require github.com/gofiber/fiber/v2 v2.38.1
+toolchain go1.21.4
+
+require github.com/gofiber/fiber/v2 v2.51.0
 
 require (
-	github.com/go-ole/go-ole v1.2.6 // indirect
-	github.com/lufia/plan9stats v0.0.0-20211012122336-39d0f177ccd0 // indirect
-	github.com/power-devops/perfstat v0.0.0-20210106213030-5aafc221ea8c // indirect
-	github.com/yusufpapurcu/wmi v1.2.2 // indirect
+	dario.cat/mergo v1.0.0 // indirect
+	github.com/bep/godartsass v1.2.0 // indirect
+	github.com/bep/godartsass/v2 v2.0.0 // indirect
+	github.com/bep/golibsass v1.1.1 // indirect
+	github.com/cli/safeexec v1.0.1 // indirect
+	github.com/cosmtrek/air v1.49.0 // indirect
+	github.com/creack/pty v1.1.21 // indirect
+	github.com/fatih/color v1.16.0 // indirect
+	github.com/fsnotify/fsnotify v1.7.0 // indirect
+	github.com/go-ole/go-ole v1.3.0 // indirect
+	github.com/gohugoio/hugo v0.120.4 // indirect
+	github.com/google/uuid v1.4.0 // indirect
+	github.com/lufia/plan9stats v0.0.0-20231016141302-07b5767bb0ed // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/mattn/go-runewidth v0.0.15 // indirect
+	github.com/pelletier/go-toml v1.9.5 // indirect
+	github.com/pelletier/go-toml/v2 v2.1.0 // indirect
+	github.com/philhofer/fwd v1.1.2 // indirect
+	github.com/power-devops/perfstat v0.0.0-20221212215047-62379fc7944b // indirect
+	github.com/rivo/uniseg v0.4.4 // indirect
+	github.com/spf13/afero v1.10.0 // indirect
+	github.com/tdewolff/parse/v2 v2.7.4 // indirect
+	github.com/tinylib/msgp v1.1.9 // indirect
+	github.com/tklauser/go-sysconf v0.3.12 // indirect
+	github.com/tklauser/numcpus v0.6.1 // indirect
+	github.com/yusufpapurcu/wmi v1.2.3 // indirect
+	golang.org/x/text v0.14.0 // indirect
+	google.golang.org/protobuf v1.31.0 // indirect
 )
 
 require (
-	github.com/andybalholm/brotli v1.0.4 // indirect
-	github.com/klauspost/compress v1.15.11 // indirect
-	github.com/shirou/gopsutil/v3 v3.22.9
+	github.com/andybalholm/brotli v1.0.6 // indirect
+	github.com/klauspost/compress v1.17.3 // indirect
+	github.com/shirou/gopsutil/v3 v3.23.10
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
-	github.com/valyala/fasthttp v1.40.0 // indirect
+	github.com/valyala/fasthttp v1.51.0 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
-	golang.org/x/sys v0.0.0-20220928140112-f11e5e49a4ec // indirect
+	golang.org/x/sys v0.14.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,23 +1,33 @@
 package main
 
 import (
-	"time"
+	"fmt"
 	"math"
+	"time"
+
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/limiter"
+	"github.com/shirou/gopsutil/v3/cpu"
 	"github.com/shirou/gopsutil/v3/mem"
 )
 
 func round(num float64) int {
-    return int(num + math.Copysign(0.5, num))
+	return int(num + math.Copysign(0.5, num))
 }
 
 func toFixed(num float64, precision int) float64 {
-    output := math.Pow(10, float64(precision))
-    return float64(round(num * output)) / output
+	output := math.Pow(10, float64(precision))
+	return float64(round(num*output)) / output
 }
 
-
+func stringInSlice(a string, list []string) bool {
+	for _, b := range list {
+		if b == a {
+			return true
+		}
+	}
+	return false
+}
 
 func main() {
 	app := fiber.New()
@@ -29,16 +39,40 @@ func main() {
 		// weightOfPreviousWindow = previous window's amount request * (whenNewWindow / Expiration)
 		// rate = weightOfPreviousWindow + current window's amount request.
 		LimitReached: func(c *fiber.Ctx) error {
-			return c.Status(403).JSON(fiber.Map{"message": "You've reached the rate limit. Please be patient!", "extra_details": fiber.Map{"IP:": c.IP(), "timeleft": c.GetRespHeader("Retry-After")}})
+			return c.Status(403).JSON(fiber.Map{"message": "You've reached the rate limit. Please be patient!", "extra_details": fiber.Map{"IP:": c.IP(), "timeLeft": c.GetRespHeader("Retry-After")}})
 		},
 	}))
 
 	app.Get("/mem", func(c *fiber.Ctx) error {
 		v, _ := mem.VirtualMemory() // Use mem from the import.
-        var used_percentage = toFixed(v.UsedPercent, 1)
-        var usedRam = v.Used/1000/1000 // Calculates it approximately to gigabytes from bytes.
-        return c.JSON(fiber.Map{"used_percent": used_percentage, "ram_usage_mb": usedRam})
+		var usedPercentage = toFixed(v.UsedPercent, 1)
+		var usedRam = v.Used / 1000 / 1000 // Calculates it approximately to gigabytes from bytes.
+		return c.JSON(fiber.Map{"used_percent": usedPercentage, "ram_usage_mb": usedRam})
 	})
 
-	app.Listen(":3000")
+	app.Get("/cpu/percent", func(c *fiber.Ctx) error {
+		v, _ := cpu.Percent(0, false) // CPU Usage
+		var usedPercentage = v[0]
+		var cleanUsedPercentage = toFixed(v[0], 1)
+
+		//TODO: Refactor as Switch Case
+		if c.Get("detailed") == "0" {
+			return c.JSON(fiber.Map{"usage_percentage": cleanUsedPercentage})
+		}
+		if c.Get("detailed") == "1" {
+			return c.JSON(fiber.Map{"detailed_usage_percentage": usedPercentage})
+		}
+		if c.Get("detailed") == "2" {
+			return c.JSON(fiber.Map{"detailed_usage_percentage": usedPercentage, "usage_percentage": cleanUsedPercentage})
+		}
+		// If no option is passed, just send both regardless.
+		return c.JSON(fiber.Map{"detailed_usage_percentage": usedPercentage, "usage_percentage": cleanUsedPercentage})
+
+	})
+
+	err := app.Listen(":3000")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 }
