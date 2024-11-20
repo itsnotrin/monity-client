bea2944060481e239400b732ca461f2322e5b008
itsnotrin <ryanwfun@gmail.com>
2024-11-20 08:58:09 +0000

    Add more useful endpoints

--- main.go
+++ main.go
@@ -36,9 +36,17 @@ func main() {
 
 	app.Get("/mem", func(c *fiber.Ctx) error {
 		v, _ := mem.VirtualMemory()
+		swap, _ := mem.SwapMemory()
 		var usedPercentage = toFixed(v.UsedPercent, 1)
 		var usedRam = v.Used / 1000 / 1000 // Calculates it approximately to gigabytes from bytes.
-		return c.JSON(fiber.Map{"used_percent": usedPercentage, "ram_usage_mb": usedRam})
+		return c.JSON(fiber.Map{
+			"used_percent": usedPercentage,
+			"ram_usage_mb": usedRam,
+			"total_mb":     v.Total / 1000 / 1000,
+			"free_mb":      v.Free / 1000 / 1000,
+			"cached_mb":    v.Cached / 1000 / 1000,
+			"swap_used_mb": swap.Used / 1000 / 1000,
+		})
 	})
 
 	app.Get("/disk", func(c *fiber.Ctx) error {
@@ -54,6 +62,9 @@ func main() {
 				"total_gb":     usage.Total / 1024 / 1024 / 1024,
 				"used_gb":      usage.Used / 1024 / 1024 / 1024,
 				"available_gb": usage.Free / 1024 / 1024 / 1024,
+				"fs_type":      partition.Fstype,
+				"inodes_used":  usage.InodesUsed,
+				"inodes_free":  usage.InodesFree,
 			})
 		}
 		return c.JSON(fiber.Map{"disks": diskInfo})
@@ -93,12 +104,17 @@ func main() {
 		uptime := time.Duration(info.Uptime) * time.Second
 
 		return c.JSON(fiber.Map{
-			"hostname": info.Hostname,
-			"os":       info.OS,
-			"platform": info.Platform,
-			"kernel":   info.KernelVersion,
-			"uptime":   uptime.String(),
-			"host_id":  info.HostID,
+			"hostname":       info.Hostname,
+			"os":             info.OS,
+			"platform":       info.Platform,
+			"kernel":         info.KernelVersion,
+			"uptime":         uptime.String(),
+			"host_id":        info.HostID,
+			"procs":          info.Procs,
+			"os_version":     info.PlatformVersion,
+			"host_arch":      runtime.GOARCH,
+			"go_version":     runtime.Version(),
+			"num_goroutines": runtime.NumGoroutine(),
 		})
 	})
 
@@ -106,6 +122,47 @@ func main() {
 		return c.JSON(fiber.Map{
 			"status":    "healthy",
 			"timestamp": time.Now().Format(time.RFC3339),
+		})
+	})
+
+	app.Get("/load", func(c *fiber.Ctx) error {
+		avgStat, _ := cpu.Times(false)
+		return c.JSON(fiber.Map{
+			"user":       toFixed(avgStat[0].User, 2),
+			"system":     toFixed(avgStat[0].System, 2),
+			"idle":       toFixed(avgStat[0].Idle, 2),
+			"nice":       toFixed(avgStat[0].Nice, 2),
+			"iowait":     toFixed(avgStat[0].Iowait, 2),
+			"irq":        toFixed(avgStat[0].Irq, 2),
+			"softirq":    toFixed(avgStat[0].Softirq, 2),
+			"steal":      toFixed(avgStat[0].Steal, 2),
+			"guest":      toFixed(avgStat[0].Guest, 2),
+			"guest_nice": toFixed(avgStat[0].GuestNice, 2),
+		})
+	})
+
+	app.Get("/metrics", func(c *fiber.Ctx) error {
+		var m runtime.MemStats
+		runtime.ReadMemStats(&m)
+
+		return c.JSON(fiber.Map{
+			"alloc":         m.Alloc,
+			"total_alloc":   m.TotalAlloc,
+			"sys":           m.Sys,
+			"num_gc":        m.NumGC,
+			"goroutines":    runtime.NumGoroutine(),
+			"gc_pause_ns":   m.PauseTotalNs,
+			"heap_objects":  m.HeapObjects,
+			"heap_alloc":    m.HeapAlloc,
+			"heap_sys":      m.HeapSys,
+			"heap_idle":     m.HeapIdle,
+			"heap_released": m.HeapReleased,
+			"stack_in_use":  m.StackInuse,
+			"stack_sys":     m.StackSys,
+			"mspan_in_use":  m.MSpanInuse,
+			"mspan_sys":     m.MSpanSys,
+			"mcache_in_use": m.MCacheInuse,
+			"mcache_sys":    m.MCacheSys,
 		})
 	})
 
